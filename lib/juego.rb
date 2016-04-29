d1755605acc6110317cd1ffac5123d6da884dbcb
flopezfdv <francisco.lopez@fdvsolutions.com>
2016-04-29 15:43:09 -0300

    ultimo dado7

--- lib/juego.rb
+++ lib/juego.rb
@@ -2,9 +2,19 @@ class Juego
 	
 	def initialize dado
 		@dado = dado
+        @valor_dado_1 = nil
+		@valor_dado_2 = nil
+
 	end
 
 	def atacar
-		@dado.tirar > @dado.tirar
+        @valor_dado_1 = @dado.tirar
+        @valor_dado_2 = @dado.tirar
+		@valor_dado_1 > @valor_dado_2
+         
+	end
+
+    def ultimos_dados
+		[@valor_dado_1, @valor_dado_2]
 	end
 end
--- spec/juego_spec.rb
+++ spec/juego_spec.rb
@@ -6,16 +6,25 @@ describe "Juego" do
 	
 	it "jugador 1 tira dado 1 y jugador 2 tira dado 6, jugador 1 pierde" do
 		juego = Juego.new (DadoMock.new(1, 6)) 
-		juego.atacar
+		juego.atacar.should == false
 	end
 
 	it "jugador 1 tira dado 6 y jugador 2 tira dado 1, jugador 2 pierde" do
 		juego = Juego.new (DadoMock.new(6, 1)) 
-		juego.atacar
+		juego.atacar.should == true
 	end
 
 	it "jugador 1 tira dado 6 y jugador 2 tira dado 6, jugador 1 pierde" do
 		juego = Juego.new (DadoMock.new(6, 6)) 
-		juego.atacar
+		juego.atacar.should == false
 	end
+
+	it "jugador 1 tira dado 4 y jugador 2 tira dado 3 los ultimos dados son 4 y 3" do
+		juego = Juego.new (DadoMock.new(4, 3)) 
+		juego.atacar
+        juego.ultimos_dados.should == [4, 3]
+	end
+    
+    
+
 end
