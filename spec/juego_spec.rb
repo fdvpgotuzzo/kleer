7f2d8d59ddc3e270bbb9deee2eff467c517857c8
flopezfdv <francisco.lopez@fdvsolutions.com>
2016-04-29 13:01:07 -0300

    spec juego ataques

--- spec/juego_spec.rb
+++ spec/juego_spec.rb
@@ -8,4 +8,14 @@ describe "Juego" do
 		juego.atacar(1,6).should == "defensor"
 	end
 
+	it "atacante tira dado 6 y defensor tira dado 1, defensor pierde" do
+		juego = Juego.new
+		juego.atacar(6,1).should == "atacante"
+	end
+
+	it "atacante tira dado 6 y defensor tira dado 6, atacante pierde" do
+		juego = Juego.new
+		juego.atacar(6,6).should == "defensor"
+	end
+
 end
